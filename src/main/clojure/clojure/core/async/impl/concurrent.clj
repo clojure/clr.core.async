27f0a5eafa82eccbffdc5fc1997b909f983c0ef4
David Miller <dmiller2718@gmail.com>
2025-11-30 15:18:51 -0500

    Add simple implementation of AtomicReferenceArray

--- src/main/clojure/clojure/core/async/impl/concurrent.clj
+++ src/main/clojure/clojure/core/async/impl/concurrent.clj
@@ -68,3 +68,98 @@
 (defonce
   ^{:doc "Number of processors reported by the JVM"}
   processors Environment/ProcessorCount)                   ;;; (.availableProcessors (Runtime/getRuntime))
+  
+  
+;; Implementation of a substitute for java.util.concurrent.atomic.AtomicReferenceArray
+;; Would love to do this lock-free (using Interlocked, Volatile.Read/Write, but can't.  See comment below.
+
+(definterface IAtomicReferenceArray
+   (set [^long idx o])
+   (get [^long idx]))
+   
+(deftype AtomicReferenceArray [^|Object[]| arr]   
+  IAtomicReferenceArray
+  (set [this ^long idx o] 
+     (locking arr
+	    (aset arr idx o)
+		o))
+  (get [this ^long idx]
+      (locking arr
+	     (aget arr idx))))
+		 
+		 
+     
+  
+
+
+
+
+;; Compliments of Copilot
+;; Problem, we cannot do the equivalent of 'ref _array[i]' in Clojure
+;; We'll have to do locking.
+;/// <summary>
+;/// Thread-safe array of references where each element can be updated atomically.
+;/// </summary>
+;public class AtomicReferenceArray<T> where T : class
+;{
+;    private readonly T[] _array
+;
+;    public AtomicReferenceArray(int length)
+;    {
+;        if (length < 0)
+;            throw new ArgumentOutOfRangeException(nameof(length), "Length must be non-negative.");
+;        _array = new T[length];
+;    }
+;
+;    public AtomicReferenceArray(T[] source)
+;    {
+;        if (source == null)
+;            throw new ArgumentNullException(nameof(source));
+;        _array = new T[source.Length];
+;        Array.Copy(source, _array, source.Length);
+;    }
+;
+;    public int Length => _array.Length;
+;
+;    /// <summary>
+;    /// Gets the value at the given index.
+;    /// </summary>
+;    public T Get(int index)
+;    {
+;        ValidateIndex(index);
+;        return Volatile.Read(ref _array[index]);
+;    }
+;
+;    /// <summary>
+;    /// Sets the value at the given index.
+;    /// </summary>
+;    public void Set(int index, T newValue)
+;    {
+;        ValidateIndex(index);
+;        Volatile.Write(ref _array[index], newValue);
+;    }
+;
+;    /// <summary>
+;    /// Atomically sets the value to newValue if the current value equals expectedValue.
+;    /// </summary>
+;    public bool CompareAndSet(int index, T expectedValue, T newValue)
+;    {
+;        ValidateIndex(index);
+;        return Interlocked.CompareExchange(ref _array[index], newValue, expectedValue) == expectedValue;
+;    }
+;
+;    /// <summary>
+;    /// Atomically gets the current value and sets a new value.
+;    /// </summary>
+;    public T GetAndSet(int index, T newValue)
+;    {
+;        ValidateIndex(index);
+;        return Interlocked.Exchange(ref _array[index], newValue);
+;    }
+;
+;    private void ValidateIndex(int index)
+;    {
+;        if (index < 0 || index >= _array.Length)
+;            throw new ArgumentOutOfRangeException(nameof(index));
+;    }
+;}  
